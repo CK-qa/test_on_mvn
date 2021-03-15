95da0e2d19dc747504460bafd1d8f7bfe55c6a86
Vika Pewpew1 <victoria.bozhko@gmail.com>
2021-03-15 13:35:39 +0300

    second commit with failing test

--- src/test/java/C1Test.java
+++ src/test/java/C1Test.java
@@ -1,14 +1,30 @@
-import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class C1Test {
 
     C1 c1 = new C1();
 
 
-    @org.junit.jupiter.api.Test
-    void plus() {
-        Assert.assertEquals("Ololo", c1.plus(), 7);
+//    @Disabled
+//    @Test
+//    void plus() {
+//        assertEquals(c1.plus(), 7, "Ololo");
+//    }
+
+    //todo
+
+    @Test
+    void always_pass() {
+        assertEquals(1, 1, "Passed");
     }
+
+//    @Test
+//    void always_fail() {
+//        assertTrue(1 == 2, "Fail!");
+//    }
 }
