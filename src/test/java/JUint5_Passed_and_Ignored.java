6575f3f7510400ba519fa9032e713b12ced8aedd
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-03-22 12:35:07 +0300

    commit1
    Signed-off-by: viktoria.bozhko <viktoria.bozhko@jetbrains.com>

--- src/test/java/JUint5_Passed_and_Ignored.java
+++ src/test/java/JUint5_Passed_and_Ignored.java
@@ -1,3 +1,5 @@
+
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -7,10 +9,27 @@ public class JUint5_Passed_and_Ignored {
 
     C2 c2 = new C2();
 
+
     @Test
     public void test_passed() {
         assertTrue("Pewpew".equalsIgnoreCase(c2.s));
     }
+
+
+    void ppp() {
+        for (int i = 0; i < 1; i++) {
+
+        }
+    }
+
+//    void ppp() {
+//        for (int i = 0; i < 1; i++) {
+//
+//        }
+//    }
+
+
+    //fixme
 
     @Disabled
     @Test
@@ -18,5 +37,5 @@ public class JUint5_Passed_and_Ignored {
         assertTrue("Pewpew".equalsIgnoreCase(c2.s));
     }
 
-    //todo
+    //todo ololo
 }
