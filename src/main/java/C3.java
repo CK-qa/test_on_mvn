eeb35621c6abc5a05a3a05f969bd34ffce3882a3
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-03-25 13:15:19 +0300

    ololo
    Signed-off-by: viktoria.bozhko <viktoria.bozhko@jetbrains.com>

--- src/main/java/C3.java
+++ src/main/java/C3.java
@@ -1,4 +1,4 @@
 public class C3 {
 
-    //122
+    //122change
 }
---
+++ src/main/java/C4.java
@@ -0,0 +1,6 @@
+public class C4 {
+
+    public static void main(String[] args) {
+        System.out.println("pewpew");
+    }
+}
