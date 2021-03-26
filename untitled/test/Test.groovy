f4ab2da3ec39ef8532ff617b5c8409cb16d27c49
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-03-26 12:53:53 +0300

    Commit message headline. Commit message body.
    Signed-off-by: viktoria.bozhko <viktoria.bozhko@jetbrains.com>

--- untitled/test/Test.groovy
+++ untitled/test/Test.groovy
@@ -1,10 +1,10 @@
-import static org.junit.Assert.*
+import static org.junit.Assert.assertEquals
 
 class Test {
 
     @org.junit.Test
     void JUnitAssertion() {
-        def expected = 32
+        def expected = 3
         def actual = 1 + 2
         assertEquals(expected, actual)
     }
